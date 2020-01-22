c551bb679d4665000051af354b27b248b8a1de48
gumdropsteve <warobson@gmail.com>
2020-01-22 05:04:10 -0800

    lists of urls to groups by city

---
+++ city_lists.py
@@ -0,0 +1,34 @@
+"""
+collection of urls to meetup groups by city/geo location
+"""
+
+# austin, tx
+austin_groups = ['https://www.meetup.com/Data-Science-ODSC-Austin/',
+				 'https://www.meetup.com/North-Austin-Pythonistas/']
+                 
+# san francisco (bay area), ca
+sfo_groups = ['https://www.meetup.com/San-Francisco-Python-Drinkup/',
+			  'https://www.meetup.com/sfpython/',
+			  'https://www.meetup.com/East-Bay-Python/',
+			  'https://www.meetup.com/Python-for-Bioinformatics/',
+			  'https://www.meetup.com/San-Francisco-Python-Hack-Night/',
+			  'https://www.meetup.com/All-Things-Python/',
+			  'https://www.meetup.com/SV-Python-Workshops/',
+			  'https://www.meetup.com/Pyninsula-Python-Peninsula-Meetup/',
+			  'https://www.meetup.com/Stanford-Python-Meetup/',
+			  'https://www.meetup.com/silicon-valley-python/',
+			  'https://www.meetup.com/Python-discussion-for-beginners/',
+			  'https://www.meetup.com/The-San-Francisco-Flask-Meetup-Group/',
+			  'https://www.meetup.com/The-San-Francisco-Django-Meetup-Group/',
+			  'https://www.meetup.com/BAyPIGgies/',
+			  'https://www.meetup.com/San-Francisco-ODSC/',
+			  'https://www.meetup.com/PyLadiesSF/',
+			  'https://www.meetup.com/San-Francisco-PyData/',
+			  'https://www.meetup.com/software/',
+			  'https://www.meetup.com/SFTwisted/',
+			  'https://www.meetup.com/Silicon-Valley-ODSC/',
+			  'https://www.meetup.com/Oakland-PyLadies/',
+			  'https://www.meetup.com/trivalleycoders/',
+			  'https://www.meetup.com/Hack-and-Learn/',
+			  'https://www.meetup.com/python-beginners/',
+			  'https://www.meetup.com/Santa-Cruz-Python/']
