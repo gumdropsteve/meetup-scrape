974a06682a7ac6d5bfe160b391028740df54709f
gumdropsteve <warobson@gmail.com>
2020-01-22 10:43:26 -0800

    add houston

--- city_lists.py
+++ city_lists.py
@@ -4,7 +4,25 @@ collection of urls to meetup groups by city/geo location
 
 # austin, tx
 austin_groups = ['https://www.meetup.com/Data-Science-ODSC-Austin/',
-				 'https://www.meetup.com/North-Austin-Pythonistas/']
+				 'https://www.meetup.com/North-Austin-Pythonistas/',
+				 'https://www.meetup.com/PyLadies-ATX/',
+				 'https://www.meetup.com/austinwebpythonusergroup/',
+				 'https://www.meetup.com/AustinLearnPython/',
+				 'https://www.meetup.com/PyData-Austin/',
+				 'https://www.meetup.com/North-Austin-Pythonistas/',
+				 'https://www.meetup.com/Aspiring-Data-Scientist-Community/',
+				 'https://www.meetup.com/austinpython/']
+# houston, tx
+houston_groups = ['https://www.meetup.com/python-14/',
+				  'https://www.meetup.com/Houston-Data-Science/',
+				  'https://www.meetup.com/ODSC-Houston-Data-Science/',
+				  'https://www.meetup.com/Houston_PyLadies/',
+				  'https://www.meetup.com/PyData-Houston/',
+				  'https://www.meetup.com/Byte-Academy-Houston-Python-FinTech-Blockchain/',
+				  'https://www.meetup.com/North-Houston-Data-Analytics-and-Machine-Learning/',
+				  'https://www.meetup.com/GA-Houston-Coding-Data-Design-Digital-Marketing/',
+				  'https://www.meetup.com/Flatiron-School-Houston-Coding-Community/',
+				  'https://www.meetup.com/Houston-Scrum-Users-Group/']
                  
 # san francisco (bay area), ca
 sfo_groups = ['https://www.meetup.com/San-Francisco-Python-Drinkup/',
@@ -31,4 +49,6 @@ sfo_groups = ['https://www.meetup.com/San-Francisco-Python-Drinkup/',
 			  'https://www.meetup.com/trivalleycoders/',
 			  'https://www.meetup.com/Hack-and-Learn/',
 			  'https://www.meetup.com/python-beginners/',
-			  'https://www.meetup.com/Santa-Cruz-Python/']
+			  'https://www.meetup.com/Santa-Cruz-Python/',
+			  'https://www.meetup.com/Women-Who-Code-SF/',
+			  'https://www.meetup.com/SF-Bay-ACM/']
